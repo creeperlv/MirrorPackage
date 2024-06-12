e4cdcc15c4fba770b365cf1baf9a86ffda1770d9
Creeper Lv <creeperlv@users.noreply.github.com>
2024-06-12 20:09:33 +1000

    Updated Mirror to 89.8.1 release.

--- Init.sh
+++ Init.sh
@@ -1,12 +1,22 @@
-wget https://github.com/MirrorNetworking/Mirror/archive/refs/tags/v89.6.8.zip -O mirror.zip
-unzip mirror.zip -d Temp -q
-mv Temp/*/Assets/Mirror ./Runtime/
-mv Temp/*/Assets/Mirror.meta ./Runtime/
-mv ./Runtime/Editor/ ./Editor/
-mv Temp/*/Assets/ScriptTemplates ./Editor/
-mv Temp/*/Assets/ScriptTemplates.meta ./Editor/
-mv ./Runtime/Tests/ ./Tests/
-mv ./Runtime/Examples ./Samples~/
-mv Temp/*/LICENSE ./LICENSE
-rm ./Temp -rf
-rm mirror.zip
+rm mirror.zip -f
+wget https://github.com/MirrorNetworking/Mirror/archive/refs/tags/v89.8.1.zip -O mirror.zip
+echo "Decompress to a temporary folder..."
+	unzip mirror.zip -d Temp -q
+echo "Removing old Mirror source code..."
+	rm ./Runtime/ -rf
+	rm ./Editor/ -rf
+	rm Tests -rf
+	rm ./Samples~/ -rf
+echo "Moving Contents..."
+	mv Temp/*/Assets/Mirror ./Runtime/
+	mv Temp/*/Assets/Mirror.meta ./Runtime/
+	mv ./Runtime/Editor/ ./Editor/
+	mv Temp/*/Assets/ScriptTemplates ./Editor/
+	mv Temp/*/Assets/ScriptTemplates.meta ./Editor/
+	mv ./Runtime/Tests/ ./Tests/
+	mv ./Runtime/Examples ./Samples~/
+	mv Temp/*/LICENSE ./LICENSE
+echo "Removing Temporary Files..."
+	rm ./Temp -rf
+	rm mirror.zip
+echo "Done."
